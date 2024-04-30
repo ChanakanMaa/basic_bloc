ce3228e95c53a1e0c37e17cad0cf505dddbb09ca
ChanakanMaa <45913436+ChanakanMaa@users.noreply.github.com>
2024-04-30 16:24:14 +0700

    add Events
    add event counterAdd and cunterReset

--- counter_num/lib/src/bloc/counter_a_bloc/bloc/counter_a_bloc.dart
+++ counter_num/lib/src/bloc/counter_a_bloc/bloc/counter_a_bloc.dart
@@ -8,8 +8,24 @@ part 'counter_a_state.dart';
 
 class CounterABloc extends Bloc<CounterAEvent, CounterAState> {
   CounterABloc() : super(CounterAState(count: 0)) {
-    on<CounterAEvent>((event, emit) {
-      //
+    // on = listening event อะไร
+
+    // Add Event
+    on<CounterAEventAdd>((event, emit) {
+      // สามารถอ้างถึงตัวแปร state count ได้เลย ที่อยู่ในไฟล์ counter_a_state.dart
+      // state.count = state.count + 1; -> ไม่สามารถทำได้เพราะตามหลักจะไม่ให้เปลี่ยนแปลงค่า state โดยตรง ทำได้แต่ไม่ควร
+      // ควร clone state ชุดใหม่ขึ้นมา ไม่ให้ปป.ตัวแปร state เดิมเพื่อป้องกันสภาวะ interfear(?) ex. ค่าใน array กำลัง render grid แต่มีการปป.แทรกเข้ามา ก็อาจเกิด index out of bound
+      // ตัวแปรเดิมที่กำลังทำงานอยู่ก็จะไม่ได้รับผลกระทบกับตัวแปรใหม่ที่มีการปป.
+
+      // โดยจะทำผ่านตัว emit จะเป็น argument ที่วิ่งเข้ามา
+      // ตัวคอยส่ง signal ให้ระบบรู้ว่าตัวแปร state ตัวใหม่
+      // emit(CounterAState(count: state.count + 1)); -> เขียนแบบนี้ได้แต่ code จะดูรกถ้ามีตัวแปร state หลายตัว
+      emit(state.copyWith(count: state.count + 1));
+    });
+
+    // Reset Event
+    on<CounterAEventReset>((event, emit) {
+      emit(state.copyWith(count: 0));
     });
   }
 }
--- counter_num/lib/src/bloc/counter_a_bloc/bloc/counter_a_state.dart
+++ counter_num/lib/src/bloc/counter_a_bloc/bloc/counter_a_state.dart
@@ -8,10 +8,19 @@ part of 'counter_a_bloc.dart';
 // final class CounterAInitial extends CounterAState {}
 
 class CounterAState extends Equatable {
-  // ม่ได้ assign ค่า then pass ค่า
+  // ไม่ได้ assign ค่า then pass ค่า
   final int count;
   const CounterAState({required this.count});
 
+  // นิยาม function ที่ใช้ในการ clone จากหน้า counter_a_bloc.dart
+  // ค่า sub-state ตัวใหม่ แบบ optional คือส่งเข้ามาหรือไม่ก็ได้
+  CounterAState copyWith({int? count}){
+    // ถ้าไม่มีค่าส่งเข้ามาก็ return เป็นค่า state เดิมออกไป 
+    return CounterAState(count: count ?? this.count);
+  }
+
   @override
-  List<Object> get props => [];
+  // เชื่อมโยงกับ equatable ว่าจะแทรคการปป. state ตัวแปรตัวไหนบ้าง
+  // ตัวแปรไหนที่ต้องอัพเดท UI ให้ใส่ตัวแปร state นั้น
+  List<Object> get props => [count];
 }

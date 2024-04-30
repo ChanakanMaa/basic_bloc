9cd2a30b8e9c2428e3eba7bf774b7d896fd5044e
ChanakanMaa <45913436+ChanakanMaa@users.noreply.github.com>
2024-04-30 16:35:10 +0700

    Update main.dart
    wrap Bloc widget on MaterailApp

--- counter_num/lib/main.dart
+++ counter_num/lib/main.dart
@@ -1,6 +1,11 @@
+// สร้าง higher component/widget มาหุ้มตัว MaterialApp เพื่อจะ BLoC statement ให้ component ทุกตัวที่อยู่ใต้ root สามารถเข้าถึงได้
+
+import 'dart:js';
+
+import 'package:counter_num/src/bloc/counter_a_bloc/bloc/counter_a_bloc.dart';
 import 'package:counter_num/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +16,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    final counterABloc =
+        BlocProvider<CounterABloc>(create: (context) => CounterABloc());
+    // return BlocProvider( -> สำหรับมี BLoC แค่ตัวเดียว
+    return MultiBlocProvider(
+      providers: [counterABloc],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'Home Page'),
       ),
-      home: const MyHomePage(title: 'Home Page'),
     );
   }
 }
